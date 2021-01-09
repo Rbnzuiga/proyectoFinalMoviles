8a5468b3725cf8f8c6fc25716341aa14f278fe7a
rbnzuiga <ruben.zuniga16@tectijuana.edu.mx>
2021-01-09 09:53:54 -0800

    Primer commit del proyecto

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.container2}>RECETAS</Text>
       <StatusBar style="auto" />
+
+
     </View>
   );
 }
@@ -16,6 +18,19 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    fontSize: 20,
     justifyContent: 'center',
+    marginTop: -600
+
   },
+  container2: {
+
+    fontSize: 40,
+
+
+  },
+  imagesize: {
+    width: 500,
+    height: 500,
+},
 });
